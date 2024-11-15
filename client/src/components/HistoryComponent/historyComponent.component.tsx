9b1b66ed098d059705f6c6ab593fea957a038647
vrezhMik <mikayelyan.vrezh99@gmail.com>
2024-11-16 01:11:54 +0400

    add: chat history view

---
+++ client/src/components/HistoryComponent/historyComponent.component.tsx
@@ -0,0 +1,9 @@
+import styles from "./HistoryComponent.module.scss";
+
+export default function HistoryComponent() {
+  return (
+    <div className={styles["history-element"]}>
+      <a href="">Last Chat</a>
+    </div>
+  );
+}
