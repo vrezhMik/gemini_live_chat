d1c31d5cd14c70a513567ab6a6cd8f90a6aee2d5
vrezhMik <mikayelyan.vrezh99@gmail.com>
2024-11-16 00:21:20 +0400

    add: user message ui

--- client/src/components/MessagesContainer/messagesContainer.component.tsx
+++ client/src/components/MessagesContainer/messagesContainer.component.tsx
@@ -1,5 +1,10 @@
+import ClientMessage from "../ClientMessage/clientMessage.component";
 import styles from "./MessagesContainer.module.scss";
 
 export default function MessagesContainer() {
-  return <div className={styles.messages}></div>;
+  return (
+    <div className={styles.messages}>
+      <ClientMessage />
+    </div>
+  );
 }
