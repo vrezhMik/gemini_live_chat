a51df36e11ab77b96214f535bb98d1769e6a3bc0
vrezhMik <mikayelyan.vrezh99@gmail.com>
2024-11-16 00:48:44 +0400

    server message

--- client/src/components/MessagesContainer/messagesContainer.component.tsx
+++ client/src/components/MessagesContainer/messagesContainer.component.tsx
@@ -1,10 +1,12 @@
 import ClientMessage from "../ClientMessage/clientMessage.component";
+import ServerMessage from "../ServerMessage/serverMessage.component";
 import styles from "./MessagesContainer.module.scss";
 
 export default function MessagesContainer() {
   return (
     <div className={styles.messages}>
       <ClientMessage />
+      <ServerMessage />
     </div>
   );
 }
